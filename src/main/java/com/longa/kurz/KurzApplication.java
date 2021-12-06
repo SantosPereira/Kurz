a5ca52c71ed8e166cecf3b1e49b8d28bcdc994a2
SantosPereira <pedrohenriquelemam@gmail.com>
2021-12-06 13:40:59 -0300

    Começo do projeto

---
+++ src/main/java/com/longa/kurz/KurzApplication.java
@@ -0,0 +1,13 @@
+package com.longa.kurz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KurzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KurzApplication.class, args);
+	}
+
+}
